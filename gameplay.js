a2b3472367fa3fab086936ed81d725aeab1b9a40
Vishesh Kasturia <visheshkasturia@gmail.com>
2021-04-07 21:49:44 +0530

    Add car movement

--- gameplay.js
+++ gameplay.js
@@ -12,6 +12,10 @@ let keys = {
 	'ArrowLeft': false
 };
 
+let player = {
+	'speed':5,
+}
+
 console.log(startScreen);
 
 document.addEventListener('keydown',keyDown);
@@ -27,15 +31,48 @@ function keyDown(event){
 
 function keyUp(event){
 	event.preventDefault();
-	keys[event.key]=true;
+	keys[event.key]=false;
 	console.log(event);
 	return;
 };
 
 function gamePlay(){
+	// console.log("Hey I am clicked");
+	let car = document.querySelector('.car');
 
-}
+	if(player.start){
+		if(keys.ArrowUp){
+			player.y -= player.speed;
+		}
+		if(keys.ArrowDown){
+			player.y += player.speed;
+		}
+		if(keys.ArrowLeft){
+			player.x -= player.speed;
+		}
+		if(keys.ArrowRight){
+			player.x += player.speed;
+		}
+		
+
+		car.style.top = player.y + 'px';
+		car.style.left = player.x + 'px';
+		window.requestAnimationFrame(gamePlay);
+	}
+
+};
 
 function start(){
+	gameArea.classList.remove('hide');
+	startScreen.classList.add('hide')
+	player.start = true;
 	window.requestAnimationFrame(gamePlay);
+
+	let car = document.createElement('div');
+	car.setAttribute('class','car');
+	// car.innerText= "Hey I am your car";
+	gameArea.appendChild(car);
+
+	player.x = car.offsetLeft;
+	player.y= car.offsetTop;
 };
